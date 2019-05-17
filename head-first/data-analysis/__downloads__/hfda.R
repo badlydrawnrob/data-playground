9f43d982ab4f802a4a191be8215e39290ce2f735
Rob McDonald <hello@robmcdonald.me>
2019-05-17 17:50:34 +0100

    Minor change

--- head-first/data-analysis/__downloads__/hfda.R
+++ head-first/data-analysis/__downloads__/hfda.R
@@ -1,1 +1,4 @@
-employees <- read.csv("http://www.headfirstlabs.com/books/hfda/hfda_ch09_employees.csv",header=TRUE)
+# Filepath error: https://stackoverflow.com/a/29978163/838046
+# To view dataframe, type "employees" in R console
+
+employees <- read.csv("Sites/badlydrawnrob/data-playground/head-first/data-analysis/__downloads__/hfda_ch09_employees.csv",header=TRUE)
