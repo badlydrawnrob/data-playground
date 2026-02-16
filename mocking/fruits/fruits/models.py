e4e674356d9fffcaf70806e03807509cca09c7d0
Rob <hello@robmcdonald.me>
2026-02-16 23:46:31 +0000

    Remove comments ...
    And put in other repo

--- mocking/fruits/fruits/models.py
+++ mocking/fruits/fruits/models.py
@@ -1,56 +1,15 @@
 # ------------------------------------------------------------------------------
 # Pydantic types
 # ==============================================================================
-# > Piccolo helps us turn our table models into Pydantic types
+# > See `badlydrawnrob/python-playground/building-with-fast-api` for more docs.
+#
+# Piccolo helps us turn our table models into Pydantic types
 #
 # Ideally we'd have an API model layer and a DATA model layer, but we're conflating
 # the two in this mocking example. I guess in general they can be shared, so long
 # as you're not tightly coupling your code. Remember that similar is NOT the same!
 #
-#
-# Serialization
-# -------------
-# > See Piccolo and Pydantic on serialization docs. In general it's best to create
-# > your own custom Pydantic models, as Piccolo "magic" falls down eventually.
-# 
-#     @ https://docs.pydantic.dev/latest/concepts/serialization/
-#     @ https://piccolo-orm.readthedocs.io/en/latest/piccolo/serialization/index.html
-#
-# 1. Primary keys `null=False` by default (but Foreign Keys `null=False`)
-# 2. Primary keys are hidden by default in the response (`secret=True`)
-# 3. `model_dump(exclude_unset=True)` is useful for optional fields
-#
-#
-# Flat shape -vs- nested
-# ----------------------
-# > `create_pydantic_model` works best with foreign keys in a nested style.
-# > @ https://piccolo-orm.readthedocs.io/en/latest/piccolo/serialization/index.html#nested
-# 
-# 1. Without the `nested=True` setting you get the default flat shape.
-# 2. More complicated models fall down with this "magic"; create your own.
-#
-#
-# Data integrity
-# --------------
-# > SQLite defaults to accept whatever data you give it by default ...
-# > @ https://sqlite.org/stricttables.html
-#
-# SQLite doesn't respect types unless it's a strict table. Strict tables are
-# inconvenient with Piccolo as we have helpful column types (especially when
-# using Postgres) ... ALWAYS validated the `DataModelIn` before inserting!
-#
-#
-# Required values
-# ---------------
-# > See `/tables.py` for optional and required fields. If `null=False`, the field
-# > should be required! `UUID` field (if primary key) is auto-generated.
-#
-# - @ https://piccolo-orm.readthedocs.io/en/latest/piccolo/serialization/index.html#required-fields
-#
-#
-# Errors
-# ------
-# > See `badlydrawnrob/python-playground/building-with-fast-api` for more docs.
+# Values are required by default (`null=False`).
 #
 #
 # Wishlist
